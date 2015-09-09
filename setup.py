3d789346620339c7a1a0164a7aa5d28c760f8bd7
Dustin Ingram <di@users.noreply.github.com>
2015-09-09 18:15:43 -0400

    Version 0.0.6 release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys, os
 from setuptools import setup, find_packages, Command
 from setuptools.command.test import test as TestCommand
 
-version = '0.0.5'
+version = '0.0.6'
 
 
 class PyTest(TestCommand):
