d1fc6dd74360a7aa0a190d2de5a4123ec02a5baa
Dustin Ingram <di@users.noreply.github.com>
2016-11-10 17:13:08 -0500

    Set vlad_logger level to INFO

--- vladiate/logs.py
+++ vladiate/logs.py
@@ -1,6 +1,7 @@
 import logging
 
 logger = logging.getLogger("vlad_logger")
+logger.setLevel(logging.INFO)
 sh = logging.StreamHandler()
 sh.setLevel(logging.INFO)
 sh.setFormatter(logging.Formatter('%(message)s'))
