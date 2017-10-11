add8301ae322eda00cd05101770fd2791f5b7fb3
Dustin Ingram <di@users.noreply.github.com>
2017-10-11 11:13:10 -0500

    Gracefully fail if boto is not present

--- vladiate/exceptions.py
+++ vladiate/exceptions.py
@@ -8,3 +8,12 @@ class BadValidatorException(Exception):
     def __init__(self, extra):
         message = 'Row does not contain the following unique_with fields: {}'
         super(BadValidatorException, self).__init__(message.format(extra))
+
+
+class MissingExtraException(Exception):
+    ''' Thrown when an extra dependency is missing '''
+    def __init__(self):
+        super(MissingExtraException, self).__init__(
+            'The `s3` extra is required to use the `S3File` class. Install'
+            ' it via `pip install vladiate[s3]`.'
+        )
