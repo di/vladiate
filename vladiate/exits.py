5815ae8038b8ad9adb612d4188c80fbd7d4a2de6
Dustin Ingram <di@users.noreply.github.com>
2018-02-23 09:33:07 -0600

    Simplify exit codes

--- vladiate/exits.py
+++ vladiate/exits.py
@@ -1,42 +1,8 @@
-""" A collection of exit codes that mimic that values returned by LINUX systems """
+""" A collection of exit codes that work on non-UNIX systems """
+
 import os
 
-__all__ = ['OK', 'NO_INPUT', 'UNAVAILABLE', 'DATAERR']
-_WINDOWS = 'nt'
-
-_OK_CODE = 0
-_DATAERR = 65
-_NOINPUT = 66
-_UNAVAILABLE = 69
-
-
-def on_windows():
-    return os.name == _WINDOWS
-
-
-@property
-def OK():
-    if on_windows():
-        return _OK_CODE
-    return os.EX_OK
-
-
-@property
-def DATAERR():
-    if on_windows():
-        return _DATAERR
-    return os.EX_DATAERR
-
-
-@property
-def NOINPUT():
-    if on_windows():
-        return _OK_CODE
-    return os.EX_NOINPUT
-
-
-@property
-def UNAVAILABLE():
-    if on_windows():
-        return _UNAVAILABLE
-    return os.EX_UNAVAILABLE
+OK = getattr(os, 'EX_OK', 0)
+DATAERR = getattr(os, 'EX_DATAERR', 65)
+NOINPUT = getattr(os, 'EX_NOINPUT', 66)
+UNAVAILABLE = getattr(os, 'EX_UNAVAILABLE', 69)
