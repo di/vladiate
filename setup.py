7c3e3d44b7a8f434e56ef3a4b4c714f6b8543eb6
Dustin Ingram <di@users.noreply.github.com>
2023-08-31 15:35:35 -0400

    Version 0.0.25 (#87)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-__version__ = "0.0.24"
+__version__ = "0.0.25"
 
 
 class PyTest(TestCommand):
