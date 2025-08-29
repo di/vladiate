3ebee85ec1d5c59d869d9a05b628dab30ebf8977
Jon Banafato <jon@jonafato.com>
2025-08-28 21:05:55 -0400

    Add 3.13 to test suite and Trove classifiers (#95)
    * Autoformat with black
    
    Apply autoformatting changes made by latest release of black.
    
    * Add 3.13 to test suite and Trove classifiers

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Intended Audience :: Developers",
     ],
     keywords="validate CSV vampires",
